e0a1592a8472b57c57b65497b3e63f886bed3787
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2025-05-08 12:02:59 -0600

    Fixed frontend fetching problems

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
+import EmployeeSection from "./components/EmployeeSection";
+
 function App() {
+  const employees = [
+    "Adolfo",
+    "Andreina",
+    "Alfredo",
+    "Carlos",
+    "Johan",
+    "Mariano",
+    "Miguel",
+    "Omar",
+    "Vicente"
+  ]
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div>
+      <h1 className="text-3xl font-bold text-center my-10">Entrada y Salida GreenRover</h1>
+      <div className="flex justify-center bg-gray-100 py-8">
+        <div className="grid grid-cols-3 gap-12">
+        {employees.map((name, index) => (
+         <EmployeeSection key={name + index} name={name}/>
+         ))}
+        </div>
+      </div>  
+    </div>
   )
 }
 
---
+++ client/src/components/EmployeeSection.jsx
@@ -0,0 +1,33 @@
+function EmployeeSection({name}) {
+  const onCheckIn = async (name) => {
+    await fetch('http://localhost:3001/api/checkin', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({name})
+    })
+  }
+
+  const onCheckOut = async (name) => {
+    await fetch('http://localhost:3001/api/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({name})
+    })
+  }
+
+  return (
+    <div className="box-border inline-block mx-15">
+      <h2 className="text-2xl font-bold text-center">{name}</h2>
+      <div>
+        <button onClick={() => {onCheckIn(name)}} className="mx-5 my-5 bg-purple-800 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-300 text-white px-4 py-2 rounded"> Entrada </button>
+        <button onClick={() => {onCheckOut(name)}}className="mx-5 my-5 bg-purple-800 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-300 text-white px-4 py-2 rounded"> Salida </button>
+      </div>
+    </div>
+  )
+}
+
+export default EmployeeSection
