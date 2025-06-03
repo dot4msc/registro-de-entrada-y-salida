c880f9de346ab77f87aa1dab9145330dd0d8f3a6
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2025-06-03 09:56:47 -0600

    fixing time glitch

--- server/models/Log.js
+++ server/models/Log.js
@@ -3,11 +3,12 @@ const mongoose = require("../database");
 const logSchema = new mongoose.Schema({
   name: {type: String, required: true, trim: true},
   action: {type: String, enum: ['entrada', 'salida'], required: true},
-  timestamp: {type: String, default: Intl.DateTimeFormat('es-MX', {
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZone: "America/Mexico_City"
-  }).format(Date.now())},
+  timestamp: {type: String, default: new Date().toLocaleTimeString('es-MX', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+    timeZone: 'America/Mexico_City'
+  }),
   date: {type: String, required: true}
 })
 
